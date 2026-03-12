0e97c7efac3b669ded445b2af2743d798aaad114
aonontojahan <aonontojahan@gmail.com>
2026-03-13 05:49:30 +0600

    Add WebSocket connection manager

---
+++ backend/app/websocket/manager.py
@@ -0,0 +1,28 @@
+from fastapi import WebSocket
+
+
+class ConnectionManager:
+
+    def __init__(self):
+        self.active_connections: dict[int, list[WebSocket]] = {}
+
+    async def connect(self, channel_id: int, websocket: WebSocket):
+        await websocket.accept()
+
+        if channel_id not in self.active_connections:
+            self.active_connections[channel_id] = []
+
+        self.active_connections[channel_id].append(websocket)
+
+    def disconnect(self, channel_id: int, websocket: WebSocket):
+        self.active_connections[channel_id].remove(websocket)
+
+    async def broadcast(self, channel_id: int, message: dict):
+        if channel_id not in self.active_connections:
+            return
+
+        for connection in self.active_connections[channel_id]:
+            await connection.send_json(message)
+
+
+manager = ConnectionManager()
